2bb57ec6914fa787892efd768bfce32a379d56dd
Harisudhan-Nagarajan <MY_NAME@example.com>
2022-04-09 11:21:31 +0530

    / /

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Route path="/Home">
           <Homedashboard />
         </Route>
-        <Route path="/**">
+        <Route path="/">
           <Welcome />
         </Route>
       </Switch>
